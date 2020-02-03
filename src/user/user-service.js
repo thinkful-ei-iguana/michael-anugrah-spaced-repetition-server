05d79392a68686f671a83cea60c30f2d5316e436
Anugrah Lambogo <lambogoai@gmail.com>
2020-02-03 12:55:38 -0600

    updated all files to be for spanish language

--- src/user/user-service.js
+++ src/user/user-service.js
@@ -46,7 +46,7 @@ const UserService = {
       const [languageId] = await trx
         .into('language')
         .insert([
-          { name: 'French', user_id },
+          { name: 'Spanish', user_id },
         ], ['id'])
 
       // when inserting words,
@@ -58,14 +58,14 @@ const UserService = {
         .first()
 
       const languageWords = [
-        ['entraine toi', 'practice', 2],
-        ['bonjour', 'hello', 3],
-        ['maison', 'house', 4],
-        ['développeur', 'developer', 5],
-        ['traduire', 'translate', 6],
-        ['incroyable', 'amazing', 7],
-        ['chien', 'dog', 8],
-        ['chat', 'cat', null],
+        ['practicar', 'practice', 2],
+        ['hola', 'hello', 3],
+        ['casa', 'house', 4],
+        ['desarrollador', 'developer', 5],
+        ['traducir', 'translate', 6],
+        ['increíble', 'amazing', 7],
+        ['perro', 'dog', 8],
+        ['gato', 'cat', null],
       ]
 
       const [languageHeadId] = await trx
--- test/user-endpoints.spec.js
+++ test/user-endpoints.spec.js
@@ -172,17 +172,17 @@ describe('User Endpoints', function () {
           name: 'test name',
         }
         const expectedList = {
-          name: 'French',
+          name: 'Spanish',
           total_score: 0,
           words: [
-            { original: 'entraine toi', translation: 'practice' },
-            { original: 'bonjour', translation: 'hello' },
-            { original: 'maison', translation: 'house' },
-            { original: 'développeur', translation: 'developer' },
-            { original: 'traduire', translation: 'translate' },
-            { original: 'incroyable', translation: 'amazing' },
-            { original: 'chien', translation: 'dog' },
-            { original: 'chat', translation: 'cat' },
+            { original: 'practicar', translation: 'practice' },
+            { original: 'hola', translation: 'hello' },
+            { original: 'casa', translation: 'house' },
+            { original: 'desarrollador', translation: 'developer' },
+            { original: 'traducir', translation: 'translate' },
+            { original: 'increíble', translation: 'amazing' },
+            { original: 'perro', translation: 'dog' },
+            { original: 'gato', translation: 'cat' },
           ]
         }
         return supertest(app)
